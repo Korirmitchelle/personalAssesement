9996f28d853e1ac3512b4a22f2d353f3d94beb28
Korirmitchelle <korirmitchelle@gmail.com>
2016-06-25 15:32:31 +0300

    initial commit

---
+++ scripts/korir.js
@@ -0,0 +1,11 @@
+marqueeInit({
+    uniqueid: 'mycrawler2',
+    style: {
+    },
+    inc: 5, //speed - pixel increment for each iteration of this marquee's movement
+    mouse: 'cursor driven', //mouseover behavior ('pause' 'cursor driven' or false)
+    moveatleast: 2,
+    neutral: 150,
+    savedirection: true,
+    random: true
+});
